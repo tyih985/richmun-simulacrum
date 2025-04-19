1fcff18bc2455447a57df04c17e601d3a8712c60
oklex <alexkim.347@gmail.com>
2025-04-19 14:37:19 -0700

    removing providers

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -5,8 +5,6 @@ import { ReactFlowProvider } from '@xyflow/react';
 
 import { ConfigProvider } from '@providers/configProvider';
 import { ThemeProvider } from '@providers/themeProvider';
-import { ReactFlowViewportProvider } from '@providers/flowViewportProvider';
-import { FocusProvider } from '@providers/selectedFocusProvider';
 
 type ProvidersProps = {
   children: ReactNode;
@@ -17,13 +15,9 @@ export const Providers: FC<ProvidersProps> = ({ children }: ProvidersProps) => (
     <BrowserRouter>
       <ThemeProvider>
         <Notifications position="bottom-right" zIndex={1000} limit={5} autoClose={3000} />
-        <FocusProvider>
-          <ReactFlowProvider>
-            <ReactFlowViewportProvider>
-              <Suspense>{children}</Suspense>
-            </ReactFlowViewportProvider>
-          </ReactFlowProvider>
-        </FocusProvider>
+        <ReactFlowProvider>
+          <Suspense>{children}</Suspense>
+        </ReactFlowProvider>
       </ThemeProvider>
     </BrowserRouter>
   </ConfigProvider>
