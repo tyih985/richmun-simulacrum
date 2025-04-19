f8ba6c6d538f4a67bbd5c2524d9d72d2c2c35325
oklex <alexkim.347@gmail.com>
2025-04-19 12:22:55 -0700

    updating paths

--- src/packages/firestorePaths/index.ts
+++ src/packages/firestorePaths/index.ts
@@ -1,9 +1,13 @@
 export const usersPath = (userId: string) => `users/${userId}`;
-export const userEventsPath = (userId: string) => `users/${userId}/events`;
-export const userEventPath = (userId: string, simulationId: string) => `users/${userId}/events/${simulationId}`;
+export const userEventsPath = (userId: string) => `users/${userId}/events_ref`;
+export const userEventPath = (userId: string, simulationId: string) => `users/${userId}/events_ref/${simulationId}`;
 
 export const simulationPath = (simulationId: string) => `simulations/${simulationId}`;
 
+/**
+ * The USERS sub-collection is the mapping of users to their role
+ * whether it's as a staff, or a delegate, or co-delegates, this is the canonical mapping
+ */ 
 export const simulationUsersPath = (simulationId: string) =>
   `simulations/${simulationId}/users`;
 export const simulationUserPath = (simulationId: string, userId: string) =>
@@ -14,13 +18,24 @@ export const committeesPath = (simulationId: string) =>
 export const committeePath = (simulationId: string, committeeId: string) =>
   `simulations/${simulationId}/committees/${committeeId}`;
 
-export const committeeDelegatesPath = (simulationId: string, committeeId: string) =>
-  `simulations/${simulationId}/committees/${committeeId}/delegates`;
-export const committeeDelegatePath = (
+export const committeeDelegationsPath = (simulationId: string, committeeId: string) =>
+  `simulations/${simulationId}/committees/${committeeId}/delegations`;
+export const committeeDelegationPath = (
   simulationId: string,
   committeeId: string,
   delegateId: string,
-) => `simulations/${simulationId}/committees/${committeeId}/delegates/${delegateId}`;
+) => `simulations/${simulationId}/committees/${committeeId}/delegations/${delegateId}`;
+export const delegationDirectivesPath = (
+  simulationId: string,
+  committeeId: string,
+  delegateId: string,
+) => `simulations/${simulationId}/committees/${committeeId}/delegations/${delegateId}/directives_ref`;
+export const delegationDirectivePath = (
+  simulationId: string,
+  committeeId: string,
+  delegateId: string,
+  directiveId: string,
+) => `simulations/${simulationId}/committees/${committeeId}/delegations/${delegateId}/directives_ref/${directiveId}`;
 
 export const directivesPath = (simulationId: string, committeeId: string) =>
   `simulations/${simulationId}/committees/${committeeId}/directives`;
@@ -28,10 +43,10 @@ export const directivePath = (
   simulationId: string,
   committeeId: string,
   directiveId: string,
-) => `simulations/${simulationId}/committees/${committeeId}/delegates/${directiveId}`;
+) => `simulations/${simulationId}/committees/${committeeId}/directives/${directiveId}`;
 export const directiveEditorsPath = (
   simulationId: string,
   committeeId: string,
   directiveId: string,
 ) =>
-  `simulations/${simulationId}/committees/${committeeId}/delegates/${directiveId}/editors`;
+  `simulations/${simulationId}/committees/${committeeId}/directives/${directiveId}/editors`;
