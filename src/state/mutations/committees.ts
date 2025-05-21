4c9611b02cf7786de3654b9168438b49cc9b55e9
oklex <alexkim.347@gmail.com>
2025-05-21 15:19:26 -0700

    commenting out un-used code

--- src/state/mutations/committees.ts
+++ src/state/mutations/committees.ts
@@ -1,58 +1,58 @@
-import {
-  createFirestoreDocument,
-  updateFirestoreDocument,
-} from '@packages/firestoreAsQuery';
-import { committeePath } from '@packages/firestorePaths';
-import { generateCommitteeId } from '@packages/generateIds';
-import { CommitteeType } from 'src/types/committee';
+// import {
+//   createFirestoreDocument,
+//   updateFirestoreDocument,
+// } from '@packages/firestoreAsQuery';
+// import { committeePath } from '@packages/firestorePaths';
+// import { generateCommitteeId } from '@packages/generateIds';
+// import { CommitteeType } from 'src/types/committee';
 
-type CommitteeMutationsType = {
-  create: (simulationId: string, data: CommitteeType) => void;
-  rename: (
-    simulationId: string,
-    committeeId: string,
-    data: Partial<Pick<CommitteeType, 'shortName' | 'longName'>>,
-  ) => void;
-  update: (
-    simulationId: string,
-    committeeId: string,
-    data: Partial<CommitteeType>,
-  ) => void;
-  archive: (simulationId: string, committeeId: string) => void;
-};
+// type CommitteeMutationsType = {
+//   create: (simulationId: string, data: CommitteeType) => void;
+//   rename: (
+//     simulationId: string,
+//     committeeId: string,
+//     data: Partial<Pick<CommitteeType, 'shortName' | 'longName'>>,
+//   ) => void;
+//   update: (
+//     simulationId: string,
+//     committeeId: string,
+//     data: Partial<CommitteeType>,
+//   ) => void;
+//   archive: (simulationId: string, committeeId: string) => void;
+// };
 
-export const committeeMutations = (): CommitteeMutationsType => {
-  const create = (simulationId: string, data: CommitteeType) => {
-    const committeeId = generateCommitteeId(data.shortName);
-    return createFirestoreDocument(committeePath(simulationId, committeeId), committeeId);
-  };
+// export const committeeMutations = (): CommitteeMutationsType => {
+//   const create = (simulationId: string, data: CommitteeType) => {
+//     const committeeId = generateCommitteeId(data.shortName);
+//     return createFirestoreDocument(committeePath(simulationId, committeeId), committeeId);
+//   };
 
-  const rename = (
-    simulationId: string,
-    committeeId: string,
-    data: Partial<Pick<CommitteeType, 'shortName' | 'longName'>>,
-  ) => {
-    return updateFirestoreDocument(committeePath(simulationId, committeeId), data);
-  };
+//   const rename = (
+//     simulationId: string,
+//     committeeId: string,
+//     data: Partial<Pick<CommitteeType, 'shortName' | 'longName'>>,
+//   ) => {
+//     return updateFirestoreDocument(committeePath(simulationId, committeeId), data);
+//   };
 
-  const update = (
-    simulationId: string,
-    committeeId: string,
-    data: Partial<CommitteeType>,
-  ) => {
-    return updateFirestoreDocument(committeePath(simulationId, committeeId), data);
-  };
+//   const update = (
+//     simulationId: string,
+//     committeeId: string,
+//     data: Partial<CommitteeType>,
+//   ) => {
+//     return updateFirestoreDocument(committeePath(simulationId, committeeId), data);
+//   };
 
-  const archive = (simulationId: string, committeeId: string) => {
-    return updateFirestoreDocument(committeePath(simulationId, committeeId), {
-      archived: true,
-    });
-  };
+//   const archive = (simulationId: string, committeeId: string) => {
+//     return updateFirestoreDocument(committeePath(simulationId, committeeId), {
+//       archived: true,
+//     });
+//   };
 
-  return {
-    create,
-    rename,
-    update,
-    archive,
-  };
-};
+//   return {
+//     create,
+//     rename,
+//     update,
+//     archive,
+//   };
+// };
--- src/state/mutations/simulations.ts
+++ src/state/mutations/simulations.ts
@@ -1,78 +1,78 @@
-import { batchUpdateDocuments } from '@packages/firestoreAsQuery';
-import { simulationPath, userEventPath } from '@packages/firestorePaths';
-import { generateSimulationId } from '@packages/generateIds';
-import { SimulationType } from '@types';
+// import { batchUpdateDocuments } from '@packages/firestoreAsQuery';
+// import { simulationPath, userEventPath } from '@packages/firestorePaths';
+// import { generateSimulationId } from '@packages/generateIds';
+// import { SimulationType } from '@types';
 
-type SimulationsMutationsType = {
-  create: (userId: string, data: SimulationType) => Promise<{ id: string }>;
-  update: (
-    userId: string,
-    simulationId: string,
-    data: Partial<SimulationType>,
-  ) => Promise<void>;
-  archive: (userId: string, simulationId: string) => Promise<void>;
-};
+// type SimulationsMutationsType = {
+//   create: (userId: string, data: SimulationType) => Promise<{ id: string }>;
+//   update: (
+//     userId: string,
+//     simulationId: string,
+//     data: Partial<SimulationType>,
+//   ) => Promise<void>;
+//   archive: (userId: string, simulationId: string) => Promise<void>;
+// };
 
-export const courseMutations = (): SimulationsMutationsType => {
-  const create = (userId: string, data: SimulationType) => {
-    const simId = generateSimulationId();
-    if (data.dates.start > data.dates.end) {
-      throw new Error('Start date must be before end date');
-    }
-    const newSimulation = {
-      id: simId,
-      ...data,
-    };
-    const reference = {
-      ...newSimulation,
-      participatingAs: 'admin',
-    };
+// export const courseMutations = (): SimulationsMutationsType => {
+//   const create = (userId: string, data: SimulationType) => {
+//     const simId = generateSimulationId();
+//     if (data.dates.start > data.dates.end) {
+//       throw new Error('Start date must be before end date');
+//     }
+//     const newSimulation = {
+//       id: simId,
+//       ...data,
+//     };
+//     const reference = {
+//       ...newSimulation,
+//       participatingAs: 'admin',
+//     };
 
-    return batchUpdateDocuments([
-      {
-        path: userEventPath(userId, simId),
-        data: reference,
-      },
-      {
-        path: simulationPath(simId),
-        data: data,
-      },
-    ]).then(() => ({ id: simId }));
-  };
+//     return batchUpdateDocuments([
+//       {
+//         path: userEventPath(userId, simId),
+//         data: reference,
+//       },
+//       {
+//         path: simulationPath(simId),
+//         data: data,
+//       },
+//     ]).then(() => ({ id: simId }));
+//   };
 
-  const update = (
-    userId: string,
-    simulationId: string,
-    data: Partial<SimulationType>,
-  ) => {
-    return batchUpdateDocuments([
-      {
-        path: userEventPath(userId, simulationId),
-        data,
-      },
-      {
-        path: simulationPath(simulationId),
-        data,
-      },
-    ]);
-  };
+//   const update = (
+//     userId: string,
+//     simulationId: string,
+//     data: Partial<SimulationType>,
+//   ) => {
+//     return batchUpdateDocuments([
+//       {
+//         path: userEventPath(userId, simulationId),
+//         data,
+//       },
+//       {
+//         path: simulationPath(simulationId),
+//         data,
+//       },
+//     ]);
+//   };
 
-  const archive = (userId: string, simulationId: string) => {
-    return batchUpdateDocuments([
-      {
-        path: userEventPath(userId, simulationId),
-        data: { archived: true },
-      },
-      {
-        path: simulationPath(simulationId),
-        data: { archived: true },
-      },
-    ]);
-  };
+//   const archive = (userId: string, simulationId: string) => {
+//     return batchUpdateDocuments([
+//       {
+//         path: userEventPath(userId, simulationId),
+//         data: { archived: true },
+//       },
+//       {
+//         path: simulationPath(simulationId),
+//         data: { archived: true },
+//       },
+//     ]);
+//   };
 
-  return {
-    create,
-    update,
-    archive,
-  };
-};
+//   return {
+//     create,
+//     update,
+//     archive,
+//   };
+// };
