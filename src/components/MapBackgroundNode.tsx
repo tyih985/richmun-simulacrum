506126cf3b1fe96544646d3ce76fef52dd29a345
oklex <alexkim.347@gmail.com>
2025-05-21 11:14:55 -0700

    basic background

---
+++ src/components/MapBackgroundNode.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import { Handle, NodeProps, Position, Node } from '@xyflow/react';
+
+type MapBackgroundProps = {
+  mapUrl: string;
+  height?: number;
+  width?: number;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const MapBackgroundNode = memo(({ data }: NodeProps<Node<MapBackgroundProps>>) => {
+  const {
+    height,
+    width,
+    mapUrl = 'https://media.istockphoto.com/id/1308342070/vector/city-map-navigation-location-map-with-city-street-roads-gps-navigator-vector-illustration.jpg?s=612x612&w=0&k=20&c=ZddvO4teNrq8_MsG2vszU_V44ykmJqc9anrJvgXpGSM=',
+  } = data;
+
+  return (
+    <>
+      <img src={mapUrl} alt="Map" height={height} width={width} />
+    </>
+  );
+});
--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,16 +1,43 @@
 import { ReactElement } from 'react';
 import { ReactFlow } from '@xyflow/react';
+import { MapBackgroundNode } from '@components/MapBackgroundNode';
+
+const ViewPortPadding = 200;
 
 export const MapPage = (): ReactElement => {
+  const nodeTypes = {
+    mapBackgroundNode: MapBackgroundNode,
+  };
+
+  const nodes = [
+    {
+      type: 'mapBackgroundNode',
+      id: '1',
+      data: {
+        mapUrl:
+          'https://media.istockphoto.com/id/1308342070/vector/city-map-navigation-location-map-with-city-street-roads-gps-navigator-vector-illustration.jpg?s=612x612&w=0&k=20&c=ZddvO4teNrq8_MsG2vszU_V44ykmJqc9anrJvgXpGSM=',
+        height: 1000,
+        width: 1000,
+      },
+      draggable: false,
+      position: {
+        x: 0,
+        y: 0,
+      },
+    },
+  ];
+
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
-      <ReactFlow nodes={[{
-        id: '1',
-        data: {},
-        position: {
-            x: 0, y: 0
-        }
-      }]} edges={[]} />
+      <ReactFlow
+        nodes={nodes}
+        edges={[]}
+        nodeTypes={nodeTypes}
+        translateExtent={[
+          [0 - ViewPortPadding, 0 - ViewPortPadding],
+          [1000 + ViewPortPadding, 1000 + ViewPortPadding],
+        ]}
+      />
     </div>
   );
 };
