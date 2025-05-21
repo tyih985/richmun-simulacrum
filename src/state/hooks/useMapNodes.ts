e4816fec52f6e8a73cbc1979fe1e8ea1eb3a9e08
oklex <alexkim.347@gmail.com>
2025-05-21 14:42:51 -0700

    adding a prototype listener

---
+++ src/state/hooks/useMapNodes.ts
@@ -0,0 +1,59 @@
+import { useFirestoreCollectionQuery } from '@packages/firestoreAsQuery/firestoreSubscription';
+import {
+  committeeMapNodesPath,
+  committeeMapBackgroundNodesPath,
+} from '@packages/firestorePaths';
+import { useMemo } from 'react';
+
+export const useMapNodes = (
+  committeeId: string,
+  mapId: string,
+  options: { enabled?: boolean; sortBy?: boolean | string } = {},
+) => {
+  const { enabled = true, sortBy = false } = options;
+
+  const nodesQuery = useFirestoreCollectionQuery(
+    committeeId && mapId ? committeeMapNodesPath(committeeId, mapId) : '',
+    {
+      enabled: !!committeeId && !!mapId && enabled,
+      sortBy,
+    },
+  );
+
+  const backgroundNodesQuery = useFirestoreCollectionQuery(
+    committeeId && mapId ? committeeMapBackgroundNodesPath(committeeId, mapId) : '',
+    {
+      enabled: !!committeeId && !!mapId && enabled,
+      sortBy,
+    },
+  );
+
+  const isLoading = nodesQuery.isLoading || backgroundNodesQuery.isLoading;
+  const isError = nodesQuery.isError || backgroundNodesQuery.isError;
+  const error = nodesQuery.error || backgroundNodesQuery.error;
+
+  const combinedNodes = useMemo(() => {
+    const regularNodes = nodesQuery.data || [];
+
+    const processedBackgroundNodes = (backgroundNodesQuery.data || []).map((node) => ({
+      ...node,
+      type: 'background',
+      draggable: false,
+      selectable: false,
+    }));
+
+    return [...regularNodes, ...processedBackgroundNodes];
+  }, [nodesQuery.data, backgroundNodesQuery.data]);
+
+  const result = useMemo(
+    () => ({
+      nodes: combinedNodes,
+      isLoading,
+      isError,
+      error,
+    }),
+    [combinedNodes, isLoading, isError, error],
+  );
+
+  return result;
+};
